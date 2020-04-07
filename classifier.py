ba229201fd04cd439779f7a4eec0100620849bbc
Attali <b00770893@essec.edu>
2020-04-07 18:11:58 +0200

    Trying Bert

--- classifier.py
+++ classifier.py
@@ -26,6 +26,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 from tqdm import tqdm
 from nltk.tokenize import  word_tokenize
+import torch
 tqdm.pandas(desc="progress-bar")
 
 
@@ -33,8 +34,8 @@ nlp = spacy.load("en_core_web_sm")
 
 nltk.download('averaged_perceptron_tagger')
 
-os.chdir("C://Users/arimo/OneDrive/Documents/Ariel/Education/ESSEC Centrale/Cours/CentraleSupelec/Elective Classes/NLP/Assignments/Assignment2/Ressources/exercise2/data") # Directory de Ariel
-#os.chdir("C://Users/33652/Documents/cours_centrale/Second_semestre/nlp/NLP_2/exercise2/data") # Directory de Raphaël
+#os.chdir("C://Users/arimo/OneDrive/Documents/Ariel/Education/ESSEC Centrale/Cours/CentraleSupelec/Elective Classes/NLP/Assignments/Assignment2/Ressources/exercise2/data") # Directory de Ariel
+os.chdir("C://Users/33652/Documents/cours_centrale/Second_semestre/nlp/NLP_2/exercise2/data") # Directory de Raphaël
 #os.chdir("/Users/michaelallouche/Google Drive/Ecoles/CentraleSupelec/Data Science Electives/Natural Language Processing/Assignment 2/exercise2/data") # Directory de Michaël
 os.getcwd()
 
@@ -216,6 +217,269 @@ train_vecs_w2v = pd.DataFrame(train_vecs_w2v)
 
 #test_vecs_w2v = np.concatenate([buildWordVector(z, n_dim) for z in tqdm(map(lambda x: x.words, X_test_tagged))])
 #test_vecs_w2v = scale(test_vecs_w2v)
+
+
+
+########################BERT MODEL ########################################################
+
+from transformers import BertTokenizer
+from torchtext import data
+
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+len(tokenizer.vocab)
+
+
+init_token = tokenizer.cls_token
+eos_token = tokenizer.sep_token
+pad_token = tokenizer.pad_token
+unk_token = tokenizer.unk_token
+
+print(init_token, eos_token, pad_token, unk_token)
+
+init_token_idx = tokenizer.convert_tokens_to_ids(init_token)
+eos_token_idx = tokenizer.convert_tokens_to_ids(eos_token)
+pad_token_idx = tokenizer.convert_tokens_to_ids(pad_token)
+unk_token_idx = tokenizer.convert_tokens_to_ids(unk_token)
+
+
+def tokenize_and_cut(sentence):
+    tokens = tokenizer.tokenize(sentence) 
+    tokens = tokens[:max_input_length-2]
+    return tokens
+
+
+TEXT = data.Field(batch_first = True,
+                  use_vocab = False,
+                  tokenize = tokenize_and_cut,
+                  preprocessing = tokenizer.convert_tokens_to_ids,
+                  init_token = init_token_idx,
+                  eos_token = eos_token_idx,
+                  pad_token = pad_token_idx,
+                  unk_token = unk_token_idx)
+
+LABEL = data.LabelField(dtype = torch.float)
+
+train_data_bert=train_data[["commentary","label"]]
+
+train_data, test_data = train_data_bert.splits(TEXT, LABEL)
+
+train_data, valid_data = train_data.split(random_state = random.seed(SEED))
+
+
+X_train_bert, X_test_bert, y_train_bert, y_test_bert = train_test_split(train_data.commentary,train_data.label, test_size=0.2)
+
+
+LABEL.build_vocab(X_train_bert)
+print(LABEL.vocab.stoi)
+
+
+
+####################################BERT MODEL 2##############################################
+# install
+
+# BERT imports
+import torch
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+from pytorch_pretrained_bert import BertTokenizer, BertConfig
+from pytorch_pretrained_bert import BertAdam, BertForSequenceClassification
+from tqdm import tqdm, trange
+import pandas as pd
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+
+#
+# queries are stored in the variable query_data_train
+# correct intent labels are stored in the variable labels
+#
+df=train_data
+# Create sentence and label lists
+sentences = df.commentary.values
+
+# We need to add special tokens at the beginning and end of each sentence for BERT to work properly
+sentences = ["[CLS] " + sentence + " [SEP]" for sentence in sentences]
+labels = df.label.values
+
+# Tokenize with BERT tokenizer
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+
+tokenized_texts = [tokenizer.tokenize(sent) for sent in sentences]
+print ("Tokenize the first sentence:")
+print (tokenized_texts[0])
+
+# Set the maximum sequence length. The longest sequence in our training set is 47, but we'll leave room on the end anyway. 
+# In the original paper, the authors used a length of 512.
+MAX_LEN = 128
+
+# Use the BERT tokenizer to convert the tokens to their index numbers in the BERT vocabulary
+input_ids = [tokenizer.convert_tokens_to_ids(x) for x in tokenized_texts]
+
+# Pad our input tokens
+input_ids = pad_sequences(input_ids, maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+# Create attention masks
+attention_masks = []
+
+# Create a mask of 1s for each token followed by 0s for padding
+for seq in input_ids:
+  seq_mask = [float(i>0) for i in seq]
+  attention_masks.append(seq_mask)
+  
+# Use train_test_split to split our data into train and validation sets for training
+
+train_inputs, validation_inputs, train_labels, validation_labels = train_test_split(input_ids, labels, 
+                                                            random_state=2018, test_size=0.1)
+train_masks, validation_masks, _, _ = train_test_split(attention_masks, input_ids,
+                                             random_state=2018, test_size=0.1)
+
+# Convert all of our data into torch tensors, the required datatype for our model
+
+train_inputs = torch.tensor(train_inputs)
+validation_inputs = torch.tensor(validation_inputs)
+train_labels = torch.tensor(train_labels)
+validation_labels = torch.tensor(validation_labels)
+train_masks = torch.tensor(train_masks)
+validation_masks = torch.tensor(validation_masks)
+
+# Select a batch size for training. For fine-tuning BERT on a specific task, the authors recommend a batch size of 16 or 32
+batch_size = 32
+
+# Create an iterator of our data with torch DataLoader. This helps save on memory during training because, unlike a for loop, 
+# with an iterator the entire dataset does not need to be loaded into memory
+
+train_data = TensorDataset(train_inputs, train_masks, train_labels)
+train_sampler = RandomSampler(train_data)
+train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=batch_size)
+
+validation_data = TensorDataset(validation_inputs, validation_masks, validation_labels)
+validation_sampler = SequentialSampler(validation_data)
+validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=batch_size)
+
+#Load BertForSequenceClassification, the pretrained BERT model with a single linear classification layer on top. 
+
+model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=3)
+#model.cuda()
+
+
+param_optimizer = list(model.named_parameters())
+no_decay = ['bias', 'gamma', 'beta']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.01},
+    {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+     'weight_decay_rate': 0.0}
+]
+
+# This variable contains all of the hyperparemeter information our training loop needs
+optimizer = BertAdam(optimizer_grouped_parameters,
+                     lr=2e-5,
+                     warmup=.1)
+
+# Function to calculate the accuracy of our predictions vs labels
+def flat_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+t = [] 
+
+# Store our loss and accuracy for plotting
+train_loss_set = []
+
+# Number of training epochs (authors recommend between 2 and 4)
+epochs = 4
+
+# trange is a tqdm wrapper around the normal python range
+for _ in trange(epochs, desc="Epoch"):
+  
+  
+  # Training
+  
+  # Set our model to training mode (as opposed to evaluation mode)
+  model.train()
+  
+  # Tracking variables
+  tr_loss = 0
+  nb_tr_examples, nb_tr_steps = 0, 0
+  
+  # Train the data for one epoch
+  for step, batch in enumerate(train_dataloader):
+    # Add batch to GPU
+    batch = tuple(t for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    # Clear out the gradients (by default they accumulate)
+    optimizer.zero_grad()
+    # Forward pass
+    loss = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+    train_loss_set.append(loss.item())    
+    # Backward pass
+    loss.backward()
+    # Update parameters and take a step using the computed gradient
+    optimizer.step()
+    
+    
+    # Update tracking variables
+    tr_loss += loss.item()
+    nb_tr_examples += b_input_ids.size(0)
+    nb_tr_steps += 1
+
+  print("Train loss: {}".format(tr_loss/nb_tr_steps))
+    
+    
+  # Validation
+
+  # Put model in evaluation mode to evaluate loss on the validation set
+  model.eval()
+
+  # Tracking variables 
+  eval_loss, eval_accuracy = 0, 0
+  nb_eval_steps, nb_eval_examples = 0, 0
+
+  # Evaluate data for one epoch
+  for batch in validation_dataloader:
+    # Add batch to GPU
+    batch = tuple(t for t in batch)
+    # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+    # Telling the model not to compute or store gradients, saving memory and speeding up validation
+    with torch.no_grad():
+      # Forward pass, calculate logit predictions
+      logits = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+    
+    # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+
+    tmp_eval_accuracy = flat_accuracy(logits, label_ids)
+    
+    eval_accuracy += tmp_eval_accuracy
+    nb_eval_steps += 1
+
+  print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
